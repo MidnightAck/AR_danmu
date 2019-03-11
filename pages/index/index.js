cf65ffa89fd43999fdc3fcd3f23dfebcd26a6fe6
CrowFea <wendaoliu40@outlook.com>
2019-03-11 22:19:52 +0800

    file upload

---
+++ pages/index/index.js
@@ -0,0 +1,57 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    height: 360
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.getSystemInfo({
+      success: res => {
+        this.setData({ height: res.windowHeight });
+      }
+    });
+  },
+  goShow: function (ev) {
+    wx.navigateTo({
+      url: '../show/show'
+    });
+  },
+  goRecognition: function (ev) {
+    wx.navigateTo({
+      url: '../recognition/recognition'
+    });
+  },
+
+
+  //事件处理函数
+
+
+  onShow: function() {
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
+
+  onLoad: function () {
+    if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    }
+
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,112 @@
+// pages/login/login.js
+const util = require('../../utils/util.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 按钮点击触发后的逻辑操作
+   */
+  sendRequest: function (e) {
+    // 
+    if (e) {
+      var data = {
+        name:'晨风'
+      }
+      util.safeRequest('/user/test', data, (data)=>{
+        console.log(data)
+
+      })
+      
+    }
+
+  },
+
+  /**
+   * wx.login
+   */
+  wxlogin: function (e) {
+    // 
+    if (e) {
+      var data = {
+        name: '晨风'
+      }
+      util.wxlogin(function(flag){
+        console.log(flag)
+      })
+    }
+  },
+
+  /**
+   * shopGoods
+   
+  shopGoods: function (e) {
+    // 
+    if (e) {
+      wx.navigateTo({
+        url: '/packageShop/pages/goods/goods'
+      })
+    }
+  },
+*/
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
